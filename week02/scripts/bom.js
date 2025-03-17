f711e828f65cee44ed7261b3bd8b8e2dbd605967
treb-dev <basseyp20@outlook.com>
2025-03-17 16:23:38 +0100

    bom

---
+++ week02/scripts/bom.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const input = document.querySelector('#favchap');
+    const button = document.querySelector('button');
+    const list = document.querySelector('#list');
+
+    button.addEventListener('click', function () {
+        if (input.value.trim() !== '') {
+            const li = document.createElement('li');
+            const deleteButton = document.createElement('button');
+
+            li.textContent = input.value;
+            deleteButton.textContent = '❌';
+
+            deleteButton.addEventListener('click', function () {
+                list.removeChild(li);
+            });
+
+            li.append(deleteButton);
+            list.append(li);
+            input.value = '';
+            input.focus();
+        } 
+    });
+});
